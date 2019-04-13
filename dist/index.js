2ed3f54d7d64bd75d0788283f75373e7788c423f
5pecia1 <pdpxpd@gmail.com>
2019-04-14 01:13:00 +0900

    make fb login

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const port = 3000;
+const puppeteer_1 = __importDefault(require("puppeteer"));
+const dotenv_1 = require("dotenv");
+dotenv_1.config();
+const fbID = process.env.FACEBOOK_ID || '';
+const fbPW = process.env.FACEBOOK_PW || '';
 function run() {
-    app.get('/', (req, res) => res.send('Hello World!'));
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    return __awaiter(this, void 0, void 0, function* () {
+        const browser = yield puppeteer_1.default.launch({
+            headless: false,
+            slowMo: 250,
+            devtools: true,
+        });
+        const page = yield browser.newPage();
+        yield page.goto('https://facebook.com');
+        yield page.type('#email', fbID);
+        yield page.type('#pass', fbPW);
+        yield page.click('#u_0_2');
+        // await browser.close();
+    });
 }
 exports.run = run;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import express from 'express';
-const app = express()
-const port = 3000
+import puppeteer from 'puppeteer';
+import { config } from 'dotenv';
 
-export function run() {
-    app.get('/', (req, res) => res.send('Hello World!'))
+config();
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const fbID: string = process.env.FACEBOOK_ID || '';
+const fbPW: string = process.env.FACEBOOK_PW|| '';
+
+export async function run() {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 250,
+        devtools: true,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://facebook.com');
+    await page.type('#email', fbID);
+    await page.type('#pass', fbPW);
+    await page.click('#u_0_2');
+        // await browser.close();
 }
