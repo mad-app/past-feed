083360f46824d5f67367a9c5043700ec056664b1
5pecia1 <pdpxpd@gmail.com>
2019-04-14 03:06:12 +0900

    add auto scroll

--- dist/index.js
+++ dist/index.js
@@ -20,15 +20,58 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const browser = yield puppeteer_1.default.launch({
             headless: false,
-            slowMo: 250,
+            // slowMo: 100,
             devtools: true,
         });
         const page = yield browser.newPage();
-        yield page.goto('https://facebook.com');
-        yield page.type('#email', fbID);
-        yield page.type('#pass', fbPW);
-        yield page.click('#u_0_2');
+        yield page.setViewport({
+            width: 1200,
+            height: 800
+        });
+        yield page.goto('https://m.facebook.com');
+        yield page.type('#m_login_email', fbID);
+        yield page.type('#m_login_password', fbPW);
+        yield page.click('#u_0_5');
+        const notNow = '#root > div._7om2 > div > div > div._7om2._2pip > div:nth-child(1) > div > div';
+        yield page.waitForSelector(notNow);
+        yield page.click(notNow);
+        yield autoScroll(page);
+        // await page.
+        //#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_ps_0_0_e > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_ps_0_0_f > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_ps_0_0_g > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_p_1 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_u_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_11_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_q_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_q_8 > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_s_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#u_s_c > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        //#MNewsFeed > div > div > div > span
+        // There are no more posts to show right now.
         // await browser.close();
     });
 }
 exports.run = run;
+function autoScroll(page) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield page.evaluate(`(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 1000);
+        });
+    })()`);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,58 @@ const fbPW: string = process.env.FACEBOOK_PW|| '';
 export async function run() {
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 250,
+        // slowMo: 100,
         devtools: true,
     });
     const page = await browser.newPage();
-    await page.goto('https://facebook.com');
-    await page.type('#email', fbID);
-    await page.type('#pass', fbPW);
-    await page.click('#u_0_2');
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+    await page.goto('https://m.facebook.com');
+    await page.type('#m_login_email', fbID);
+    await page.type('#m_login_password', fbPW);
+    await page.click('#u_0_5');
+
+    const notNow = '#root > div._7om2 > div > div > div._7om2._2pip > div:nth-child(1) > div > div';
+    await page.waitForSelector(notNow)
+    await page.click(notNow);
+    await autoScroll(page);
+    // await page.
+//#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_ps_0_0_e > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_ps_0_0_f > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_ps_0_0_g > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_p_1 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_u_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_11_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+
+//#u_q_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_q_8 > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+
+//#u_s_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#u_s_c > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+//#MNewsFeed > div > div > div > span
+// There are no more posts to show right now.
         // await browser.close();
+}
+
+async function autoScroll(page: puppeteer.Page){
+    await page.evaluate(`(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 1000);
+        });
+    })()`);
 }
