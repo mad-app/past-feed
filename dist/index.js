cc7d902fe5b8e28bbda0e3254246a2984e766ccf
5pecia1 <pdpxpd@gmail.com>
2019-04-14 17:11:54 +0900

    make to get post address

--- dist/index.js
+++ dist/index.js
@@ -14,13 +14,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = __importDefault(require("puppeteer"));
 const dotenv_1 = require("dotenv");
 dotenv_1.config();
+const IS_DEV = process.env.DEV == "true" || false;
 const fbID = process.env.FACEBOOK_ID || '';
 const fbPW = process.env.FACEBOOK_PW || '';
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         const browser = yield puppeteer_1.default.launch({
             headless: false,
-            // slowMo: 100,
+            slowMo: 10,
             devtools: true,
         });
         const page = yield browser.newPage();
@@ -30,34 +31,54 @@ function run() {
         });
         yield page.goto('https://m.facebook.com');
         yield page.type('#m_login_email', fbID);
-        yield page.type('#m_login_password', fbPW);
-        yield page.click('#u_0_5');
+        const pw = '#m_login_password';
+        try {
+            yield page.type(pw, fbPW);
+            yield page.click('#u_0_5');
+        }
+        catch (e) {
+            const emailLogin = '#login_form > div._2pie > div:nth-child(1) > button';
+            yield page.waitForSelector(emailLogin);
+            yield page.click(emailLogin);
+            yield page.waitForSelector(pw);
+            yield page.type(pw, fbPW);
+            yield page.click('#u_0_6');
+        }
         const notNow = '#root > div._7om2 > div > div > div._7om2._2pip > div:nth-child(1) > div > div';
         yield page.waitForSelector(notNow);
         yield page.click(notNow);
+        yield page.waitFor('#u_0_w'); //my profile
         yield autoScroll(page);
-        // await page.
-        //#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_ps_0_0_e > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_ps_0_0_f > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_ps_0_0_g > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_p_1 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_u_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_11_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_q_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_q_8 > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#u_s_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+        // NOTE: Theree is one more div in the internal post.
         //#u_s_c > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-        //#MNewsFeed > div > div > div > span
-        // There are no more posts to show right now.
+        const postATagSelector = 'div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a';
+        const postATags = yield page.$$(postATagSelector);
+        const aTagHrefs = [];
+        for (let i = 1; i < postATags.length; i++) {
+            const aTag = postATags[i];
+            const tags = yield page.evaluate(e => e.href, aTag);
+            aTagHrefs.push(tags);
+        }
+        // const postATags = await page.$$eval(postATagSelector, es => {
+        //     console.log(es)
+        //     return es.map(e => {
+        //         console.log((<any>e).href); return e
+        //     })
+        // });
+        // const postURLs = await postATags.map(async e => await (await e.getProperty('textContent')).jsonValue())
+        console.log(aTagHrefs);
+        console.log(1232131);
+        // await page.
         // await browser.close();
     });
 }
 exports.run = run;
+//TODO: check "There are no more posts to show right now" message.#MNewsFeed > div > div > div > span 
 function autoScroll(page) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield page.evaluate(`(async () => {
+        const interval = IS_DEV ? 30 : 1000;
+        yield sleep(IS_DEV ? 1000 : 5000);
+        yield page.evaluate(`(async (interval) => {
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
             var distance = 100;
@@ -66,12 +87,15 @@ function autoScroll(page) {
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight){
+                if(totalHeight >= scrollHeight || totalHeight >= 6000){
                     clearInterval(timer);
                     resolve();
                 }
-            }, 1000);
+            }, 50);
         });
-    })()`);
+    })()`, interval);
     });
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { config } from 'dotenv';
 
 config();
 
+const IS_DEV: boolean = process.env.DEV == "true" || false;
 const fbID: string = process.env.FACEBOOK_ID || '';
-const fbPW: string = process.env.FACEBOOK_PW|| '';
+const fbPW: string = process.env.FACEBOOK_PW || '';
 
 export async function run() {
     const browser = await puppeteer.launch({
         headless: false,
-        // slowMo: 100,
+        slowMo: 10,
         devtools: true,
     });
     const page = await browser.newPage();
@@ -19,35 +20,58 @@ export async function run() {
     });
     await page.goto('https://m.facebook.com');
     await page.type('#m_login_email', fbID);
-    await page.type('#m_login_password', fbPW);
-    await page.click('#u_0_5');
+    const pw = '#m_login_password';
+    try {
+        await page.type(pw, fbPW);
+        await page.click('#u_0_5');
+    } catch (e) {
+        const emailLogin = '#login_form > div._2pie > div:nth-child(1) > button';
+        await page.waitForSelector(emailLogin)
+        await page.click(emailLogin)
+
+        await page.waitForSelector(pw)
+        await page.type(pw, fbPW);
+        await page.click('#u_0_6');
+    }
 
     const notNow = '#root > div._7om2 > div > div > div._7om2._2pip > div:nth-child(1) > div > div';
+
     await page.waitForSelector(notNow)
     await page.click(notNow);
+
+    await page.waitFor('#u_0_w');//my profile
     await autoScroll(page);
+
+    // NOTE: Theree is one more div in the internal post.
+    //#u_s_c > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
+    const postATagSelector = 'div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a';
+    const postATags = await page.$$(postATagSelector);
+    const aTagHrefs = [];
+    for (let i = 1; i < postATags.length; i++) {
+        const aTag = postATags[i];
+        const tags = await page.evaluate(e => e.href, aTag);
+        aTagHrefs.push(tags);
+    }
+    // const postATags = await page.$$eval(postATagSelector, es => {
+    //     console.log(es)
+    //     return es.map(e => {
+    //         console.log((<any>e).href); return e
+    //     })
+    // });
+
+    // const postURLs = await postATags.map(async e => await (await e.getProperty('textContent')).jsonValue())
+
+    console.log(aTagHrefs);
+    console.log(1232131);
     // await page.
-//#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_ps_0_0_2 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_ps_0_0_e > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_ps_0_0_f > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_ps_0_0_g > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_p_1 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_u_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_11_3 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-
-//#u_q_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_q_8 > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-
-//#u_s_4 > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#u_s_c > div > div > header > div._4g34._5i2i._52we > div > div > div._4g34 > div > a
-//#MNewsFeed > div > div > div > span
-// There are no more posts to show right now.
-        // await browser.close();
+    // await browser.close();
 }
 
-async function autoScroll(page: puppeteer.Page){
-    await page.evaluate(`(async () => {
+//TODO: check "There are no more posts to show right now" message.#MNewsFeed > div > div > div > span 
+async function autoScroll(page: puppeteer.Page) {
+    const interval = IS_DEV ? 30 : 1000;
+    await sleep(IS_DEV ? 1000 : 5000);
+    await page.evaluate(`(async (isDev, interval) => {
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
             var distance = 100;
@@ -56,11 +80,16 @@ async function autoScroll(page: puppeteer.Page){
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight){
+                if(totalHeight >= scrollHeight || (isDev && totalHeight >= 6000){
                     clearInterval(timer);
                     resolve();
                 }
-            }, 1000);
+            }, 50);
         });
-    })()`);
+    })()`, IS_DEV, interval);
 }
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
