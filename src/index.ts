c768196f53e740990e968c86fb9a883a964473c3
5pecia1 <pdpxpd@gmail.com>
2019-04-17 07:11:24 +0900

    clean code

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import crawl from './crawl';
 
 config();
 
-const IS_DEV: boolean = process.env.DEV == "true" || false;
 const fbID: string = process.env.FACEBOOK_ID || '';
 const fbPW: string = process.env.FACEBOOK_PW || '';
 
@@ -33,32 +32,5 @@ export async function run() {
     })
     console.log(1232131);
     // await page.
-    // await browser.close();
+    await browser.close();
 }
-
-//TODO: check "There are no more posts to show right now" message.#MNewsFeed > div > div > div > span 
-async function autoScroll(page: puppeteer.Page) {
-    const interval = IS_DEV ? 30 : 1000;
-    await sleep(IS_DEV ? 1000 : 5000);
-    await page.evaluate(`(async (isDev, interval) => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if(totalHeight >= scrollHeight || (isDev && totalHeight >= 6000){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 50);
-        });
-    })()`, IS_DEV, interval);
-}
-
-function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
